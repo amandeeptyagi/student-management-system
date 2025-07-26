82e76e1d18dcfa670d3d78145591bc8dfc9be9f0
amandeep tyagi <199378217+amandeeptyagi@users.noreply.github.com>
2025-07-26 13:50:02 +0530

    some configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// 👇 Load env based on current mode or fallback to 'development'
+const env = loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +16,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: env.VITE_PROXY_URL,
+        changeOrigin: true,
+        secure: false
+      }
+    },
+    host: true, // or '0.0.0.0'
+    port: 5173,
+  }
 })
